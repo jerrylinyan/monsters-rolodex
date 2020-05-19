2011005ecea1d923370fbf074fc3708a09d4d3c8
Andrew <andrew@Andrews-MacBook-Pro.local>
2020-05-19 20:03:41 +1000

    Updates

---
+++ precache-manifest.e0f053aaa37d25de3dfd331401cb4c6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0500919512ce3e9d69c576a3165782b7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5591d6ce8917008aeef7",
+    "url": "/monsters-rolodex/static/css/main.23167fff.chunk.css"
+  },
+  {
+    "revision": "e8743bf758ecf001cdf3",
+    "url": "/monsters-rolodex/static/js/2.2a6eed2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.2a6eed2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5591d6ce8917008aeef7",
+    "url": "/monsters-rolodex/static/js/main.47263887.chunk.js"
+  },
+  {
+    "revision": "68b00582aea0f61137cd",
+    "url": "/monsters-rolodex/static/js/runtime-main.4c13341c.js"
+  }
+]);
